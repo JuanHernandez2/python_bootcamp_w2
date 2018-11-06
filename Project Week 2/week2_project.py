ed7110246d4ae00a1fca3626706ab41ca1d27c8e
JuanHernandez2 <juanfhr.d@gmail.com>
2018-11-06 02:34:12 -0500

    All docStrings and comments added

--- Project Week 2/week2_project.py
+++ Project Week 2/week2_project.py
@@ -17,7 +17,8 @@ class Contact:
 
     @classmethod
     def default_contact(cls):
-            c = cls(" "," "," ", " ", " "," ")
+            """Class method for creating an empty Contact in a fast way"""
+            c = cls(" "," "," ", " ", " "," ") #empty Contact
             return c
 
 class Phone_number():
@@ -38,21 +39,21 @@ def create_contact(contacts, c):
 
 def show_sorted_contacts(contacts):
         "This function returns the contact list sorted by the date of creation sorting by microsecond"
-        contacts.sort(key = lambda x: x.date_created.microsecond)
+        contacts.sort(key = lambda x: x.date_created.microsecond) #sorting by microsecond
         return contacts
         
 def save_contacts(contacts):
     """This function save each contact of the parameter list in a file.txt"""
-    saving_file = open("Contacts.txt", "w")
+    saving_file = open("Contacts.txt", "w") #creates a file in write mode
     for i in range(0 , len(contacts)):
-        saving_file.write(str(i)+"\n")
-        saving_file.write(str(contacts[i].id) + "\n")
-        saving_file.write(contacts[i].name +"\n")
-        saving_file.write(contacts[i].lastname +"\n")
-        saving_file.write(str(contacts[i].age)+"\n")
-        saving_file.write(str(contacts[i].phone_number)+"\n")
-        saving_file.write(contacts[i].email+ "\n")
-        saving_file.write(str(contacts[i].date_created) + "\n")
+        saving_file.write(str(i)+"\n") #contact counter
+        saving_file.write(str(contacts[i].id) + "\n") #contact id
+        saving_file.write(contacts[i].name +"\n") #contact name
+        saving_file.write(contacts[i].lastname +"\n") #contact lastname
+        saving_file.write(str(contacts[i].age)+"\n") #contact age
+        saving_file.write(str(contacts[i].phone_number)+"\n") #contact phone numbers
+        saving_file.write(contacts[i].email+ "\n") #contact email
+        saving_file.write(str(contacts[i].date_created) + "\n") #date of creation
     saving_file.close()
 
 def update_contact(c):
@@ -62,18 +63,18 @@ def update_contact(c):
         c.age = input("New age?")
         numbers = [ ]
         flag = "yes"
-        while flag == "yes":
+        while flag == "yes": #when the user enters no the loop breaks
                 numbers.append(input("Enter a phone number"))
                 flag = input("Enter no if you are ready to continue")      
         c.phone_number = Phone_number(numbers)
         c.email = input("New email?")
 
 def generator_hide_contacts(contacts):
-        
+    """This generator prints all the contacts except the one that the user wants to hide"""
     id_to_hide = input("Enter contact id to hide ")
     for i in contacts:
-        if i.id is not id_to_hide:
-            yield i.id
+        if i.id is not id_to_hide: #if the contact id is different from the one to hide
+            yield i.id 
             yield i.name
             yield i.lastname
             yield i.phone_number
@@ -84,7 +85,7 @@ def hide_contact(contacts):
     g = generator_hide_contacts(contacts)
     while True:
         try:
-            print(next(g))
+            print(next(g)) #call the generator
         except StopIteration:
             break
 
@@ -97,7 +98,7 @@ if __name__ == '__main__':
     c = Contact.default_contact()
     contacts = [ ]
     
-    while option != 6 :
+    while option != 6 : # when the option is 6 the loops breaks and the module finishes
             print("Choose one number...")
             print("1. Create contact")
             print("2. Show contacts sorted by creation date")
@@ -106,14 +107,14 @@ if __name__ == '__main__':
             print("5. Hide contact")
             print("6. Exit")
             option = int(input())
-            option_dictionary = {
+            option_dictionary = {  # a dictionary with functions names
                    "1" : create_contact,
                    "2" : show_sorted_contacts,
                    "3" : save_contacts,
                    "4" : update_contact,
                    "5" : hide_contact
                 } 
-            if option == 1:
+            if option == 1: #create contact
                     id = input("Contact id? ")
                     name = input("Contact name? ")
                     lastname = input("Contact lastname? ")
@@ -126,9 +127,9 @@ if __name__ == '__main__':
                     contact_numbers = Phone_number(numbers)
                     email = input("Contact email? ")
                     c = Contact(id, name, lastname, age, contact_numbers, email)
-                    option_dictionary[str(option)](contacts,c)
-            elif option == 2:
-                    sorted_contacts = option_dictionary[str(option)](contacts)
+                    option_dictionary[str(option)](contacts,c) #calls the function with its parameters
+            elif option == 2: #show contacts sorted by creation date
+                    sorted_contacts = option_dictionary[str(option)](contacts) #calls the function with its parameters
                     i=0
                     for s in sorted_contacts:
                             print("-------------------"+ str(i) +"---------------")
@@ -142,14 +143,14 @@ if __name__ == '__main__':
                             print("-------------------------------------------\n")
                             i += 1
                     
-            elif option == 3:
-                    option_dictionary[str(option)](contacts)
-            elif option == 4:
+            elif option == 3: #save contacts
+                    option_dictionary[str(option)](contacts)#calls the function with its parameters
+            elif option == 4: #update contacts
                     id = input("Enter contact id to update")
                     contact_to_update = [x for x in contacts if x.id == id]
-                    option_dictionary[str(option)](contact_to_update[0])
-            elif option == 5: 
-                    option_dictionary[str(option)](contacts)
+                    option_dictionary[str(option)](contact_to_update[0])#calls the function with its parameters
+            elif option == 5: #hide contact
+                    option_dictionary[str(option)](contacts)#calls the function with its parameters
         
 
                  
