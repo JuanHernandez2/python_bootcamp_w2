e8fd6d5cdbc53b51d6a3e47d73340d01ef9cfb8a
JuanHernandez2 <juanfhr.d@gmail.com>
2018-11-06 01:36:20 -0500

    Verified User stories: create contact, show contacts sorted by creation date and save contacts

--- Project Week 2/week2_project.py
+++ Project Week 2/week2_project.py
@@ -2,14 +2,23 @@ import datetime
 
 class Contact:
 
-    def __init__(self, name, lastname, age, phone_number, email):
+    def __init__(self, id, name, lastname, age, phone_number, email):
             now = datetime.datetime.now()
+            self.id = id
             self.name = name
             self.lastname = lastname
             self.age = age
             self.phone_number = phone_number
             self.email = email
             self.date_created = now
+    
+    def __repr_(self):
+            return "Name: %s\n Lastname: %s\n Age: %s\n Phone number: %s\n Email: %s\n " % (self.name, self.lastname, self.age, self.phone_number, self.email)
+
+    @classmethod
+    def default_contact(cls):
+            c = cls(" "," "," ", " ", " "," ")
+            return c
 
 def create_contact(contacts, c):
         """This function create a new contact with the parameter information and then is
@@ -17,7 +26,8 @@ def create_contact(contacts, c):
         contacts.append(c)
 
 def show_sorted_contacts(contacts):
-        contacts.sort(key = lambda x: x.microsecond)
+        "This function returns the contact list sorted by the date of creation sorting by microsecond"
+        contacts.sort(key = lambda x: x.date_created.microsecond)
         return contacts
         
 def save_contacts(contacts):
@@ -33,32 +43,25 @@ def save_contacts(contacts):
         saving_file.write(str(contacts[i].date_created) + "\n")
     saving_file.close()
 
-def update_contact(contacts, name):
+def update_contact(c):
         """This function update a contact with the given name as a parameter"""
-        for c in contacts:
-                if name == c.name:
-                        c.name = input("New name?")
-                        c.lastname = input("New lastname?")
-                        c.age = input("New age?")
-                        c.phone_number = input("New phone number?")
-                        c.email = input("New email?")
+        c.name = input("New name?")
+        c.lastname = input("New lastname?")
+        c.age = input("New age?")
+        c.phone_number = input("New phone number?")
+        c.email = input("New email?")
 
-
+def hide_contact(c):
+        pass
     
 
 
 
 if __name__ == '__main__':
     option = 0
-    c = Contact(" ", " ", " ", " ", " ")
+    c = Contact.default_contact()
     contacts = [ ]
-    option_dictionary = {
-            "1" : create_contact(contacts,c),
-            "2" : show_sorted_contacts(contacts),
-            "3" : save_contacts(contacts),
-            "4" : update_contact(name),
-            "5" : hide_contact(name)
-    }  
+    
     while option != 6 :
             print("Choose one number...")
             print("1. Create contact")
@@ -68,15 +71,49 @@ if __name__ == '__main__':
             print("5. Hide contact")
             print("6. Exit")
             option = int(input())
+            option_dictionary = {
+                   "1" : create_contact,
+                   "2" : show_sorted_contacts,
+                   "3" : save_contacts,
+                   "4" : update_contact,
+                   "5" : hide_contact
+                } 
             if option == 1:
-                    name = input("Contact name")
-                    lastname = input("Contact lastname?")
-                    age = input("Contact age?")
-                    phone_number = input("Contact phone number ?")
-                    email = input("Contact email?")
-                    c = Contact(name, lastname, age, phone_number, email)
-                    option_dictionary[str(option)]
-                
+                    id = input("Contact id? ")
+                    name = input("Contact name? ")
+                    lastname = input("Contact lastname? ")
+                    age = input("Contact age? ")
+                    phone_number = input("Contact phone number? ")
+                    email = input("Contact email? ")
+                    c = Contact(id, name, lastname, age, phone_number, email)
+                    option_dictionary[str(option)](contacts,c)
+            elif option == 2:
+                    sorted_contacts = option_dictionary[str(option)](contacts)
+                    i=0
+                    for s in sorted_contacts:
+                            print("-------------------"+ str(i) +"---------------")
+                            print("Id: " + s.id)
+                            print("Name: " + s.name)
+                            print("Lastname: " + s.lastname)
+                            print("Age: " + s.age)
+                            print("Phone number: " + s.phone_number)
+                            print("Email: " + s.email)
+                            print("Creation date: " + str(s.date_created))
+                            print("-------------------------------------------\n")
+                            
+                            i += 1
+                    
+            elif option == 3:
+                    option_dictionary[str(option)](contacts)
+            elif option == 4:
+                    id = input("Enter contact id to update")
+                    contact_to_update = [x for x in contacts if x.id == id]
+                    option_dictionary[str(option)](contact_to_update[0])
+            elif option == 5: 
+                    pass
+
+                 
+
 
             
 
